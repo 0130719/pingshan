88508f734a8d5cd9bb61eadce92ac08ab7a12f64
Shawn Guo <guoshuai@cn.ibm.com>
2020-09-28 11:02:52 +0800

    updates

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-'use strict'
-
-const express = require('express');
+'use strict';
+const express = require('express'),
+    fs = require('fs');
 
 const app = express();
 
-app.get('/ppp', (req, res) => {
-    res.json({
-        a: 'a',
-        1: 1
-    });
+let dishes = JSON.parse(fs.readFileSync('data/dishes.json', 'utf8'));
+let restaurants = JSON.parse(fs.readFileSync('data/restaurants.json', 'utf8'));
+let dishNames = [];
+
+
+for (let dishKey in dishes) {
+    dishNames.push(dishKey);
+}
+
+app.use(express.static('public'));
+
+app.get('/dishes', (req, res) => {
+    let size = parseInt(req.query.size) || 10;
+    let from = parseInt(req.query.from) || 0;
+
+    let results = [];
+
+    for (let i = 0; i < size && i + from < dishNames.length; i++) {
+        results.push(dishes[dishNames[i + from]]);
+    }
+
+    res.json(results);
 });
 
-app.listen(9625, () => {
+app.get('/dish/:id', (req, res) => {
+
+    let dish = dishes[req.params.id];
+
+    if (!dish) {
+        return res.json({});
+    }
+
+    dish.restaurants_detail = [];
+
+    for (let restaurantKey of dish.restaurants) {
+        let restaurant = restaurants[restaurantKey];
+
+        if (restaurant) {
+            dish.restaurants_detail.push(restaurant);
+        }
+    }
+
+    res.json(dish);
+
+
+});
+
+
+app.listen(3001, async () => {
     console.log('Server started.');
 });
